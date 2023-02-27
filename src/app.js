f625cae96e8b996e0592b002bab4e52e3846ba89
Santiago <acquwebsa@gmail.com>
2023-02-27 20:02:44 -0300

    primer push

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { dirname } = require("path");
+const path = require ("path");
+const { env } = require("process");
+
+const app = express ();
+
+
+const port = process.env.port || 3000;
+
+app.listen(port,()=> (
+    console.log("Servidor escuchando en el puerto http://localhost:" + port)
+));
+app.use(express.static("public"));
+    
+app.get ("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+
+});
