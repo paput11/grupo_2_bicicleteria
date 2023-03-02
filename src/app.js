d78209b3126e5bf2b82ac4dc7730aadae65a3341
Santiago <acquwebsa@gmail.com>
2023-03-01 22:03:23 -0300

    armado catalogo

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.listen(port,()=> (
 app.use(express.static("public"));
     
 app.get ("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+
 
 });
