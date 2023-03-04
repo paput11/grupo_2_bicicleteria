64fef3e91718037bd505718f9d0d93a65e11f047
paput11 <paput11@gmail.com>
2023-03-04 19:17:32 -0300

    feat: pagina de contacto con header respnsive

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ app.use(express.static("public"));
     
 app.get ("/", (req,res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
+});
 
-
+app.get ("/contacto/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/contacto.html"));
 });
