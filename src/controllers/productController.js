3b04189133c1878bea972ea0063390fd369707fe
paput11 <paput11@gmail.com>
2023-07-04 18:33:27 -0300

    feat: validacion y formato backend producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,73 +1,104 @@
-const path = require ("path");
+const path = require("path");
 /* const fs = require ("fs"); */
-const db = require ("../database/models")
-const {Op} = require ("sequelize");
+const db = require("../database/models")
+const { Op } = require("sequelize");
 /* const { stringify } = require("querystring"); */
+const { body, validationResult } = require('express-validator');
 
-const productsFilePath = path.join(__dirname,"../data/products.json")
+const productsFilePath = path.join(__dirname, "../data/products.json")
 
 const productController = {
-  lista: function(req,res){
+  lista: function (req, res) {
     db.product.findAll()
-      .then (function(products){
-        res.render("catalogo",{products})
+      .then(function (products) {
+        res.render("catalogo", { products })
       })
   },
-  detalle: (req,res) => {
+  detalle: (req, res) => {
     db.product.findByPk(req.params.id)
-      .then((product)=>{res.render("detail",{product})})
+      .then((product) => { res.render("detail", { product }) })
   },
-  
-  crear: (req, res) => {
-		res.render("crearProducto")
-	},
 
-  guardar: function(req,res){
-    
-    db.product.create({
-      
-      nombre: req.body.name , 
-      descripcion: req.body.descripcion,
-      jerarquia_id: req.body.categoria,
-      precio: parseInt(req.body.precio),
-      imagen: req.file ? req.file.filename : "default-imagen.jpg",
-      color: req.body.color,
-    })
-    
-    .then(res.redirect("/catalogo"))
+  crear: (req, res) => {
+    res.render("crearProducto")
+  },
+
+  guardar: function (req, res) {
+    const result = validationResult(req).array();
+
+    //VALIDAR FORMATO DE IMAGEN
+    //■ Deberá ser un archivo válido (JPG, JPEG, PNG, GIF).
+    if (result.length) {
+      //como tiene errores => mostramos creacion de producto con los errores
+      return res.render("crearProducto", {
+        errors: [
+          ...result.map((result) => {
+            return ({
+              field: result.path,
+              msg: result.msg
+            });
+          })
+        ]
+      });
+    } else {
+      const isImageValid = () => {
+        let isValid = false;
+
+        if (req.file) {
+          const validExtentions = ["jpeg", "jpg", "gif", "png"];
+          const fileExtention = req.file.originalname.split(".")[1];
+          isValid = validExtentions.includes(fileExtention);
+          console.log({ validExtentions, fileExtention, isValid });
+        }
+
+        return isValid;
+      };
+
+      db.product.create({
+
+        nombre: req.body.name,
+        descripcion: req.body.descripcion,
+        jerarquia_id: req.body.categoria,
+        precio: parseInt(req.body.precio),
+        imagen: isImageValid() ? req.file.filename : "default-imagen.jpg",
+        color: req.body.color,
+      })
+
+        .then(res.redirect("/catalogo"))
+    }
   },
 
   modificar: (req, res) => {
     db.product.findByPk(req.params.id)
-    .then((product)=>(res.render("editarProducto", {product})))
+      .then((product) => (res.render("editarProducto", { product })))
   },
 
   editar: (req, res) => {
     let product = db.product.findByPk(req.params.id)
     const editProduct = {
-      nombre: req.body.nombre , 
+      nombre: req.body.nombre,
       descripcion: req.body.descripcion,
       jerarquia_id: req.body.categoria,
       precio: parseInt(req.body.precio),
       imagen: req.file ? req.file.filename : product.imagen,
       color: req.body.color,
     }
-    db.product.update(editProduct,{where:{id:req.params.id}})
-      .then(res.redirect ("/catalogo"));
+    db.product.update(editProduct, { where: { id: req.params.id } })
+      .then(res.redirect("/catalogo"));
   },
-  
+
   eliminar: (req, res) => {
-    db.product.destroy({where: {id: req.params.id}})
-      .then(res.redirect ("/catalogo"))
+    db.product.destroy({ where: { id: req.params.id } })
+      .then(res.redirect("/catalogo"))
       .catch(res.status(404))
   },
 
   listaApi: (req, res) => {
     db.product
       .findAll()
-      .then(productos => { 
-        
-        let products= []
+      .then(productos => {
+
+        let products = []
         for (let i = 0; i < productos.length; i++) {
           const element = {
             id: productos[i].id,
@@ -75,16 +106,16 @@ const productController = {
             descripcion: productos[i].descripcion,
             image: productos[i].imagen,
             category: productos[i].jerarquia_id,
-            detail: "http://localhost:3000/catalogo/detalleApi/"+productos[i].id,
+            detail: "http://localhost:3000/catalogo/detalleApi/" + productos[i].id,
           }
           products.push(element)
-        } 
+        }
         return products
       })
-      .then((productos)=>{
+      .then((productos) => {
         return res.status(200).json({
           total: productos.length,
-          data:  productos,
+          data: productos,
           status: 200
         })
       })
@@ -92,7 +123,8 @@ const productController = {
         console.error(error);
         return res.status(500).json({
           error: 'Error al obtener los usuarios',
-          status: 500});
+          status: 500
+        });
       });
   },
 
@@ -101,102 +133,105 @@ const productController = {
       .findByPk(req.params.id)
       .then(producto => {
         if (!producto) {
-          return res.status(404).json({ 
+          return res.status(404).json({
             error: 'Producto no encontrado',
-            status: 404 });
-        }else{
+            status: 404
+          });
+        } else {
           return res.status(200).json({
-          data: producto,
-          urlImagen: "http://localhost:3000/images/trek/"+producto.imagen,
-          status: 200
-        })};
+            data: producto,
+            urlImagen: "http://localhost:3000/images/trek/" + producto.imagen,
+            status: 200
+          })
+        };
       })
       .catch(error => {
         console.error(error);
-        return res.status(500).json({ 
+        return res.status(500).json({
           error: 'Error al obtener el Producto',
-          status: 500});
+          status: 500
+        });
       });
   },
 
 }
 
 
-  /* detail: (req,res) => { 
-    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-    const id = req.params.id
-    let product = products.filter (product => {return product.id == id})
-    let producto = product[0]
-    
-    res.render("detail",{product:producto});
-    }, */
-
-  /* store:(req,res) => { 
-    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-    
-    const newProduct = {
-      id: products[products.length-1].id + 1,
-      nombre: req.body.name , 
-      descripcion: req.body.descripcion,
-      categoria: req.body.categoria,
-      precio: parseInt(req.body.precio),
-      imagen: req.file ? req.file.filename : "default-imagen.jpg",
-      color: req.body.color,
-    }
-    products.push (newProduct)
-    let productJSON= JSON.stringify(products,null," ")
-    fs.writeFileSync(productsFilePath, productJSON)
-
-    res.redirect ("/catalogo")
- 
-  }, */
-   /*  destroy : (req, res) => {
-		let id = req.params.id;
-
-		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-		let finalProducts = products.filter(product => {
-			return product.id != id
-		})
-		
-		let productsJSON = JSON.stringify(finalProducts, null, " ");
-	
-		fs.writeFileSync(productsFilePath, productsJSON);
-	},
-   */
-  /* change: (req, res) => {
-    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-    const id = req.params.id;
-    const product = products.find(product => product.id == id);
+/* detail: (req,res) => { 
+  const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+  const id = req.params.id
+  let product = products.filter (product => {return product.id == id})
+  let producto = product[0]
   
-    const editProduct = {
-      id: parseInt(req.params.id),
-      nombre: req.body.nombre , 
-      descripcion: req.body.descripcion,
-      categoria: req.body.categoria,
-      precio: parseInt(req.body.precio),
-      imagen: req.file ? req.file.filename : product.imagen,
-      color: req.body.color,
-    }
-    let indice= products.findIndex(product=>{return product.id==id})
-    products [indice]= editProduct;
-    let productsJSON = JSON.stringify(products, null, " ");
-	
-		fs.writeFileSync(productsFilePath, productsJSON);
-
-		res.redirect ("/catalogo");
+  res.render("detail",{product:producto});
   }, */
-  /* catalogo: (req,res) => { 
-    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-    
-    res.render("catalogo",{products});
-    }, */
 
-  /* edit: (req, res) => {
-    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-    const id = req.params.id;
-    const product = products.find(product => product.id == id);
-		res.render("editarProducto", {product})
-	}, */
+/* store:(req,res) => { 
+  const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+  
+  const newProduct = {
+    id: products[products.length-1].id + 1,
+    nombre: req.body.name , 
+    descripcion: req.body.descripcion,
+    categoria: req.body.categoria,
+    precio: parseInt(req.body.precio),
+    imagen: req.file ? req.file.filename : "default-imagen.jpg",
+    color: req.body.color,
+  }
+  products.push (newProduct)
+  let productJSON= JSON.stringify(products,null," ")
+  fs.writeFileSync(productsFilePath, productJSON)
+
+  res.redirect ("/catalogo")
+ 
+}, */
+/*  destroy : (req, res) => {
+ let id = req.params.id;
+
+ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+ let finalProducts = products.filter(product => {
+   return product.id != id
+ })
+	
+ let productsJSON = JSON.stringify(finalProducts, null, " ");
+	
+ fs.writeFileSync(productsFilePath, productsJSON);
+},
+*/
+/* change: (req, res) => {
+  const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+  const id = req.params.id;
+  const product = products.find(product => product.id == id);
+ 
+  const editProduct = {
+    id: parseInt(req.params.id),
+    nombre: req.body.nombre , 
+    descripcion: req.body.descripcion,
+    categoria: req.body.categoria,
+    precio: parseInt(req.body.precio),
+    imagen: req.file ? req.file.filename : product.imagen,
+    color: req.body.color,
+  }
+  let indice= products.findIndex(product=>{return product.id==id})
+  products [indice]= editProduct;
+  let productsJSON = JSON.stringify(products, null, " ");
+	
+  fs.writeFileSync(productsFilePath, productsJSON);
+
+  res.redirect ("/catalogo");
+}, */
+/* catalogo: (req,res) => { 
+  const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+  
+  res.render("catalogo",{products});
+  }, */
+
+/* edit: (req, res) => {
+  const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+  const id = req.params.id;
+  const product = products.find(product => product.id == id);
+  res.render("editarProducto", {product})
+}, */
 
 module.exports = productController
